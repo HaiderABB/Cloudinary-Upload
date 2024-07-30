7cb14f73eb653a014cee7c611bb1835c41dd9b57
HaiderABB <haider.a.moazzam@gmail.com>
2024-07-30 15:02:22 +0500

    minor update

--- Src/Helper/CheckFileType.js
+++ Src/Helper/CheckFileType.js
@@ -3,10 +3,9 @@ const path = require('path');
 
 const CheckFileType = async (req, file, cb) => {
   const filetypes = ['.jpg', '.jpeg', '.png', '.svg'];
+
   // Check ext
-  console.log(path.extname(file.originalname).toLowerCase())
   const extname = filetypes.includes(path.extname(file.originalname).toLowerCase());
-  console.log(extname);
 
   if (extname) {
     return cb(null, true);
